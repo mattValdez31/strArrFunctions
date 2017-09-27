52065a713a7fc1a3a90459673a264727c22e97eb
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-09-26 20:27:05 -0400

    instantiated object/defined contructor & destructor

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	$obj = new main();
+
+	class main
+	{
+		public function __construct()
+		{
+			echo 'Hello i\'m an object';
+		}
+
+		public function __destruct()
+		{
+			echo 'I\'m done </br>';
+		}
+	}
+?>
